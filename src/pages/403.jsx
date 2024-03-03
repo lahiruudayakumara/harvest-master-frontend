147eefda82b4801525578cc6f1d82fa7729b2040
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-03-03 08:36:28 +0530

    add error pages

---
+++ src/pages/403.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from 'react-helmet'
+
+const Page403 = () => {
+  return (
+    <>
+        <Helmet>
+            <title> 403 Forbidden</title>
+        </Helmet>
+        <Page403 />
+    </>
+  )
+}
+
+export default Page403
---
+++ src/pages/500.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View500 } from '../section/error'
+
+const Page500 = () => {
+  return (
+    <>
+        <Helmet>
+              <title> 500 Internal Server Error</title>
+        </Helmet>
+        <View500 />
+    </>
+  )
+}
+
+export default Page500
