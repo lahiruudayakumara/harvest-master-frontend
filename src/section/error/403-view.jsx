d0aa43c41b30aee2d4b80d7f4e507dedacd48900
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-03-03 08:36:00 +0530

    add error section

---
+++ src/section/error/403-view.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const View403 = () => {
+  return (
+    <>
+        <h1>403</h1>
+    </>
+  )
+}
+
+export default View403
---
+++ src/section/error/500-view.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const View500 = () => {
+  return (
+    <>
+    <h1>500</h1>
+    </>
+  )
+}
+
+export default View500
