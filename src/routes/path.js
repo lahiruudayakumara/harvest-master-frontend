88d0cc7caafd9bfb07d5b8d9e29c848b6b32e287
N A D NIMSARA <duvindunimsara@gmail.com>
2024-03-17 01:43:55 +0530

    Add routes for inventory manager

--- src/routes/path.js
+++ src/routes/path.js
@@ -5,56 +5,68 @@ import LoginPage from "../pages/auth/admin/login";
 import DashboardLayout from "../layouts/dashboard";
 import FinancialManagerDashboard from "./section/financial-manager-dashboard";
 import FinancialManagerPayment from "../pages/financial-manager/financial-manager-payment";
+import { InventoryAddProduct } from "../pages/inventory manager/inventory-add-product";
+import InventoryManagerDashboard from "../pages/inventory manager/inventory-manager-dashboard";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        Component: Home,
-    },
-    // {
-    //     path: "/farmer",
-    //     children: [
-    //         {
-    //             index: true,
-    //             Component: Home,
-    //         },
-    
-    //     ]
-    // },
-    // {
-    //     path: "/buyer",
-    //     children: [
-    //         {
-    //             index: true,
-    //             Component: Home,
-    //         },
-    //     ]
-    // },
-    // {
-    //     path: "/admin",
-    //     children: [
-    //         {
-    //             index: true,
-    //             Component: Home,
-    //         },
-    //     ]
-    // },
-    {
-        path: "/financial-manager",
-        Component: FinancialManagerDashboard,
-        children: [
-            {
-                path: 'payment',
-                Component: FinancialManagerPayment,
-            }
-        ]
-    },
-    {
-        path: "/login",
-        Component: LoginPage,
-    },
-    {
-        path: "*",
-        Component: NotFoundPage
-    },
+  {
+    path: "/",
+    Component: Home,
+  },
+  // {
+  //     path: "/farmer",
+  //     children: [
+  //         {
+  //             index: true,
+  //             Component: Home,
+  //         },
+
+  //     ]
+  // },
+  // {
+  //     path: "/buyer",
+  //     children: [
+  //         {
+  //             index: true,
+  //             Component: Home,
+  //         },
+  //     ]
+  // },
+  // {
+  //     path: "/admin",
+  //     children: [
+  //         {
+  //             index: true,
+  //             Component: Home,
+  //         },
+  //     ]
+  // },
+  {
+    path: "/financial-manager",
+    Component: FinancialManagerDashboard,
+    children: [
+      {
+        path: "payment",
+        Component: FinancialManagerPayment,
+      },
+    ],
+  },
+  {
+    path: "/inventory-manager",
+    Component: InventoryManagerDashboard,
+    children: [
+      {
+        path: "add-product",
+        Component: InventoryAddProduct,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    Component: LoginPage,
+  },
+  {
+    path: "*",
+    Component: NotFoundPage,
+  },
 ]);
