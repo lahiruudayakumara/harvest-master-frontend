861582e3a666b32ec709114892c98dd96740157e
It22899224J <it22899224@my.sliit.lk>
2024-03-17 15:51:56 +0530

    status bar fetch

--- src/section/post-harvest/overral-status-bar.jsx
+++ src/section/post-harvest/overral-status-bar.jsx
@@ -1,7 +1,7 @@
 import { List, ListItem, ListItemText } from "@mui/material";
 import React from "react";
 
-export const OverallStatus = () => {
+export const OverallStatus = ({ status }) => {
   return (
     <>
       <List
@@ -17,29 +17,28 @@ export const OverallStatus = () => {
             mb: 1.5,
             height: 100,
             textAlign: "center",
-            
           }}
           style={{ fontWeight: "bold" }}
         >
           <ListItemText>
             <ListItemText>testw</ListItemText>
-            <ListItemText>testw</ListItemText>
+            <ListItemText>DISTRICTS</ListItemText>
+          </ListItemText>
+          <ListItemText>
+            <ListItemText>{status}</ListItemText>
+            <ListItemText>ACTIVE FIELDS</ListItemText>
           </ListItemText>
           <ListItemText>
             <ListItemText>testw</ListItemText>
-            <ListItemText>testw</ListItemText>
+            <ListItemText>HECTARES</ListItemText>
           </ListItemText>
           <ListItemText>
             <ListItemText>testw</ListItemText>
-            <ListItemText>testw</ListItemText>
+            <ListItemText>RICE VARIETIES</ListItemText>
           </ListItemText>
           <ListItemText>
             <ListItemText>testw</ListItemText>
-            <ListItemText>testw</ListItemText>
-          </ListItemText>
-          <ListItemText>
-            <ListItemText>testw</ListItemText>
-            <ListItemText>testw</ListItemText>
+            <ListItemText>Expected HARVEST (Kg)</ListItemText>
           </ListItemText>
         </ListItem>
       </List>
