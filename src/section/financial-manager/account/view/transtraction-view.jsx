b14b03b6dd1fd76ccfbfd398e96d80fe5db53092
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-03-18 20:49:56 +0530

    Add TranstractionView component

---
+++ src/section/financial-manager/account/view/transtraction-view.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TranstractionTable from '../transtraction-table'
+
+const TranstractionView = () => {
+    return (
+        <>
+            <TranstractionTable />
+        </>
+    )
+}
+
+export default TranstractionView
