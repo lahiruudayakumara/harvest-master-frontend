0162defc6c57a73366e3c02c3fca28b0862d7f10
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-03-18 14:26:57 +0530

    add financial manager overview page

---
+++ src/pages/financial-manager/financial-manager-overview.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet"
+
+
+const FinacialManagerOverview = () => {
+  return (
+    <>
+    <Helmet>
+        <title>Dashboard : Financial Manager</title>
+    </Helmet>
+    </>
+  )
+}
+
+export default FinacialManagerOverview
