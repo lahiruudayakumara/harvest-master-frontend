bbe1af7ddc17a2095cfc0f4e4bad790933df6d22
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-03-18 20:50:20 +0530

    Add payment button and draft payment section

--- src/section/financial-manager/Payment/view/payment-view.jsx
+++ src/section/financial-manager/Payment/view/payment-view.jsx
@@ -1,10 +1,33 @@
+import { Box, Button, Typography } from '@mui/material'
+import SendIcon from '@mui/icons-material/Send';
 import React from 'react'
 
 const PaymentView = () => {
   return (
-    <div>
-      <h1>Payment View</h1>
-    </div>
+    <Box flexGrow={1} >
+      <Button
+        style={{ backgroundColor: '#2CA019'}}
+        variant="contained"
+        endIcon={<SendIcon />}
+      >
+        Payment
+      </Button>
+      <Box 
+        boxShadow={2} 
+        marginTop={2}
+        padding={2}
+        borderRadius={2}
+        minHeight='70vh'
+        >
+        <Typography
+              variant="h6"
+              marginBottom={1}
+              style={{ color: "#07bc0c", fontWeight: "bolder" }}
+            >
+              Draft Payment
+          </Typography>
+      </Box>
+    </Box>
   )
 }
 
