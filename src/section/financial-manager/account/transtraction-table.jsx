3bc227f98638458f019d7cbb591d2cfe3051b379
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-03-18 14:28:50 +0530

    add transtraction table component

---
+++ src/section/financial-manager/account/transtraction-table.jsx
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import { Box } from '@mui/material';
+
+const columns = [
+    { id: 'name', label: 'Name', minWidth: 170 },
+    { id: 'account', label: 'Account\u00a0No', minWidth: 100 },
+    { id: 'date', label: 'Date', minWidth: 100 },
+    { id: 'amount', label: 'Amount(Rs)', minWidth: 100 },
+    { id: 'status', label: 'Status', minWidth: 100 },
+];
+
+function createData(name, account, date, amount, status) {
+    return { name, account, date, amount, status };
+}
+
+const rows = [
+    createData('Duvindu Nimsara', 'xxxx xxxx xxxx 1234', '2024 - 02 - 19', '15,100.00', 'Done'),
+    createData('John Smith', '1234 5678 9012 3456', '2023-10-15', '20,500.00', 'Pending'),
+    createData('Michael Brown', '2468 1357 8024 6793', '2024-03-10', '12,300.00', 'Pending'),
+    createData('Sophia Garcia', '6543 2109 8765 4321', '2024-02-28', '18,900.00', 'Processing'),
+    createData('Daniel Martinez', '1357 2468 6793 8024', '2024-03-05', '6,500.00', 'Done'),
+    createData('Olivia Taylor', '3210 9876 5432 1098', '2024-02-14', '15,750.00', 'Rejected')
+];
+
+export default function TranstractionTable() {
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+                <TableContainer sx={{ maxHeight: 440 }}>
+                    <Table stickyHeader aria-label="sticky table">
+                        <TableHead>
+                            <TableRow>
+                                {columns.map((column) => (
+                                    <TableCell
+                                        key={column.id}
+                                        align={column.align}
+                                        style={{ minWidth: column.minWidth }}
+                                    >
+                                        {column.label}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((row) => {
+                                    return (
+                                        <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                            {columns.map((column) => {
+                                                const value = row[column.id];
+                                                return (
+                                                    <TableCell key={column.id} align={column.align}>
+                                                        {column.format && typeof value === 'number'
+                                                            ? column.format(value)
+                                                            : value}
+                                                    </TableCell>
+                                                );
+                                            })}
+                                        </TableRow>
+                                    );
+                                })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[10, 25, 100]}
+                    component="div"
+                    count={rows.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </Paper>
+        </Box>
+
+    );
+}
