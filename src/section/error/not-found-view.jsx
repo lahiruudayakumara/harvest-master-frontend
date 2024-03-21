7d95550bdc6f052dc4e1ca7e6f9b9865cf0cb378
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-03-21 22:04:17 +0530

    Refactor NotFoundView component to use Material-UI components

--- src/section/error/not-found-view.jsx
+++ src/section/error/not-found-view.jsx
@@ -1,7 +1,45 @@
+import { Box, Button, Typography } from "@mui/material";
+import GppBadIcon from '@mui/icons-material/GppBad';
+import ArrowBackIcon from '@mui/icons-material/ArrowBackIos';
+
+const handleGoBack = () => {
+  window.history.back();
+};
+
 const NotFoundView = () => {
   return (
     <>
-      <h1>404 error</h1>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexGrow: 1,
+          height: '100vh',
+        }}
+      >
+        <Box sx={{ textAlign: 'center' }}>
+          <GppBadIcon sx={{ fontSize: 100, color: '#2CA019', marginBottom: 2 }} />
+          <Typography variant="h4" gutterBottom>
+            404 - Page Not Found
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            Sorry, the page you are looking for could not be found.
+          </Typography>
+          <Typography variant="body1">
+            Please check back later.
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ backgroundColor: "#2CA019", marginTop: "25px" }}
+            startIcon={<ArrowBackIcon />}
+            onClick={handleGoBack}
+          >
+            Back
+          </Button>
+        </Box>
+      </Box>
     </>
   );
 };
