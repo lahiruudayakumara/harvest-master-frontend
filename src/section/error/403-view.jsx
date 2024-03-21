a0a092f7da1342414b9852b5d750b9ef1412f299
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-03-21 22:14:35 +0530

    Refactor 403 error view component

--- src/section/error/403-view.jsx
+++ src/section/error/403-view.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { Box, Typography } from "@mui/material"
+import BlockIcon from '@mui/icons-material/Block';
+import ArrowBackIcon from '@mui/icons-material/ArrowBackIos';
 
 const View403 = () => {
   return (
     <>
-        <h1>403</h1>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexGrow: 1,
+          height: '70vh',
+        }}
+      >
+        <Box sx={{ textAlign: 'center' }}>
+          <BlockIcon sx={{ fontSize: 100, color: '#2CA019', marginBottom: 2 }} />
+          <Typography variant="h4" gutterBottom>
+            403 - Forbidden
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            Sorry, you don't have permission to access this page.
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ backgroundColor: "#2CA019", marginTop: "25px" }}
+            startIcon={<ArrowBackIcon />}
+            onClick={handleGoBack}
+          >
+            Back
+          </Button>
+        </Box>
+      </Box>
     </>
   )
 }
