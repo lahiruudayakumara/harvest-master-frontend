0db104e48b07fe03f95aba7ea9560b409776c759
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-03-21 22:25:06 +0530

    Remove unnecessary imports from home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,5 @@
 import { Helmet } from 'react-helmet'
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
 import MaintenanceView from '../section/error/maintenance-view';
-
 
 export default function Home() {
   return (
