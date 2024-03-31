8b550d480650d19918feef8406ec1b95b678fa6b
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-03-31 23:44:43 +0530

    Add admin navigation slice to reducer

--- src/stores/reducer.js
+++ src/stores/reducer.js
@@ -1,9 +1,11 @@
 // store.js
 import { combineReducers } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import adminReducer from './slices/adminNavSlice';
 
 const Reducer = combineReducers({
     auth: authReducer,
+    nav: adminReducer,
 });
 
 export default Reducer;
