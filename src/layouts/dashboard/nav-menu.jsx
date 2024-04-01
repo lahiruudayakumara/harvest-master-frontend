3a2784fc224fb1c60d493a7795b41c1a10d5f9c5
N A D NIMSARA <duvindunimsara@gmail.com>
2024-04-01 10:52:21 +0530

    ui improve

--- src/layouts/dashboard/nav-menu.jsx
+++ src/layouts/dashboard/nav-menu.jsx
@@ -1,67 +1,72 @@
-import React, { useState } from 'react';
-import { Box, List, ListItem, ListItemText, useTheme } from '@mui/material';
-import { Link } from 'react-router-dom';
-import Logo from '../../components/logo';
-import UseNavData from './config-navigation';
-import { useDispatch } from 'react-redux';
-import { navClick } from '../../stores/slices/adminNavSlice';
+import React, { useState } from "react";
+import { Box, List, ListItem, ListItemText, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import Logo from "../../components/logo";
+import UseNavData from "./config-navigation";
+import { useDispatch } from "react-redux";
+import { navClick } from "../../stores/slices/adminNavSlice";
 
 const NavMenu = ({ role }) => {
-    const theme = useTheme();
-    const [clickItem, setClickItem] = useState(0);
-    const dispatch = useDispatch();
+  const theme = useTheme();
+  const [clickItem, setClickItem] = useState(0);
+  const dispatch = useDispatch();
 
-    const menuItems = UseNavData.find(item => item.parent === role);
-    return (
-        <Box
-            display={{ xs: 'none', sm: 'block' }}
-            sx={{
-                backgroundColor: '#2CA019',
-                width: '100%',
-                height: '100%',
-                padding: 2,
-                position: 'fixed',
-                top: 0,
-                left: 0,
-                overflowY: 'auto',
-                [theme.breakpoints.up('md')]: {
-                    width: '250px',
+  const menuItems = UseNavData.find((item) => item.parent === role);
+  return (
+    <Box
+      display={{ xs: "none", sm: "block" }}
+      sx={{
+        backgroundColor: "#2CA019",
+        width: "100%",
+        height: "100%",
+        padding: 2,
+        position: "fixed",
+        top: 0,
+        left: 0,
+        overflowY: "auto",
+        [theme.breakpoints.up("md")]: {
+          width: "200px",
+        },
+      }}
+    >
+      <Logo sx={{ width: "150px", height: "auto" }} />
+
+      <List>
+        {menuItems.submenu.map((item, index) => {
+          const isActive = location.pathname === item.to;
+
+          return (
+            <ListItem
+              key={index}
+              button
+              onClick={() => {
+                setClickItem(index);
+                dispatch(navClick({ displayName: item.text }));
+              }}
+              component={Link}
+              to={item.to}
+              sx={{
+                "&:hover": {
+                  backgroundColor: isActive
+                    ? "rgba(217, 217, 217, 0.25)"
+                    : "rgba(217, 217, 217, 0.25)",
+                  borderRadius: "5px",
                 },
-            }}
-        >
-            <Logo sx={{ width: '150px', height: 'auto' }} />
-
-            <List>
-                {menuItems.submenu.map((item, index) => {
-                    const isActive = location.pathname === item.to;
-
-                    return (
-                        <ListItem
-                            key={index}
-                            button
-                            onClick={() => {
-                                setClickItem(index);
-                                dispatch(navClick({ displayName: item.text }))
-                            }}
-                            component={Link}
-                            to={item.to}
-                            sx={{
-                                '&:hover': {
-                                    backgroundColor: isActive ? 'rgba(217, 217, 217, 0.25)' : 'rgba(217, 217, 217, 0.25)',
-                                    borderRadius: '5px'
-                                },
-                                backgroundColor: clickItem === index ? 'rgba(217, 217, 217, 0.25)' : '',
-                                borderRadius: '5px'
-                            }}
-                        >
-                            <span style={{ color: 'white', marginRight: '8px' }}>{item.icon && item.icon}</span>
-                            <ListItemText primary={item.text} style={{ color: 'white' }} />
-                        </ListItem>
-                    );
-                })}
-            </List>
-        </Box>
-    );
+                backgroundColor:
+                  clickItem === index ? "rgba(217, 217, 217, 0.25)" : "",
+                borderRadius: "5px",
+              }}
+            >
+              <span style={{ color: "white", marginRight: "8px" }}>
+                {item.icon && item.icon}
+              </span>
+              <ListItemText primary={item.text} style={{ color: "white" }} />
+            </ListItem>
+          );
+        })}
+      </List>
+    </Box>
+  );
 };
 
 export default NavMenu;
