7d911ec535206c25e4a26b56feda77f0442b4099
Dulashi1 <dulashi123456@gmail.com>
2024-04-01 12:55:01 +0530

    instructor fix

--- src/section/instructor/account/view/account-view.jsx
+++ src/section/instructor/account/view/account-view.jsx
@@ -1,11 +1,12 @@
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
+import InquiriesTable from "../../inquiries/inquiries-table";
 
 const AccountView = () => {
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
-        <Typography>Instrucor Dashboard</Typography>
+      <InquiriesTable />
       </Box>
     </>
   );
