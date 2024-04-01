f6ffbe2f65bdb2909c425fefadc3a4033979f793
N A D NIMSARA <duvindunimsara@gmail.com>
2024-04-01 20:16:52 +0530

    Refactor imports and add inventory reducer

--- src/stores/reducer.js
+++ src/stores/reducer.js
@@ -1,12 +1,13 @@
 // store.js
-import { combineReducers } from '@reduxjs/toolkit';
-import authReducer from './slices/authSlice';
-import adminReducer from './slices/adminNavSlice';
+import { combineReducers } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice";
+import adminReducer from "./slices/adminNavSlice";
+import inventoryReducer from "./slices/inventorySlice";
 
 const Reducer = combineReducers({
-    auth: authReducer,
-    nav: adminReducer,
+  auth: authReducer,
+  nav: adminReducer,
+  inventory: inventoryReducer,
 });
 
 export default Reducer;
-
