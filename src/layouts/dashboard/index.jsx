6a1d01795ce4bd47a47075b422c2b9eb8a491b5c
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-04-01 08:59:33 +0530

    Refactor DashboardLayout component to display user role dynamically

--- src/layouts/dashboard/index.jsx
+++ src/layouts/dashboard/index.jsx
@@ -1,17 +1,36 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { useSelector } from 'react-redux'
+
 import Header from './header'
 import Main from './main'
 import NavMenu from './nav-menu'
+import { selectNav } from '../../stores/slices/adminNavSlice'
+import { selectAuth } from '../../stores/slices/authSlice'
 
-const DashboardLayout = ({children, role }) => {
+const DashboardLayout = ({ children, role }) => {
+  const { displayName } = useSelector(selectNav);
+  const { userRole, user } = useSelector(selectAuth);
+
+  let updateUserRole = userRole;
+
+  if (userRole == "ROLE_ADMIN") {
+    updateUserRole = "Admin"
+  } else if (userRole == "ROLE_FINANCIAL_MANAGER") {
+    updateUserRole = "Financial Manager"
+  } else if (userRole == "ROLE_LOGISTIC_HANDLER") {
+    updateUserRole = "Logistic Handler"
+  } else if (userRole == "ROLE_INSTRUCTOR") {
+    updateUserRole = "Instructor"
+  } else if (userRole == "ROLE_INVENTORY") {
+    updateUserRole = "Inventory"
+  }
+
   return (
     <>
-     <Header page="Dashboard" role={role} name="LAHIRU"   />
-     <NavMenu role={role} />
-     <Main>
+      <Header page={displayName} role={updateUserRole} name={user.name} />
+      <NavMenu role={updateUserRole} />
+      <Main>
         {children}
-     </Main>
+      </Main>
     </>
   )
 }
