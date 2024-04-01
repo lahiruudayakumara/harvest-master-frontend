ef9a273ba30ae0fb88ef8ffa66c22068b7ae2e22
Dulashi1 <dulashi123456@gmail.com>
2024-04-01 12:28:03 +0530

    fix view inquiry

---
+++ src/section/instructor/inquiries/view/inquiries-view.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import InquiriesTable from '../inquiries-table'
+import { Box } from '@mui/material'
+
+const InquiriesView = () => {
+    return (
+        <Box>
+            <InquiriesTable />
+        </Box>
+    )
+}
+
+export default InquiriesView
