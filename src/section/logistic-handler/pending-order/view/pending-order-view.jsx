82dafa1273c793d6881a180130edf08eee2a4784
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2024-04-02 10:35:26 +0530

    implement pending order view

--- src/section/logistic-handler/pending-order/view/pending-order-view.jsx
+++ src/section/logistic-handler/pending-order/view/pending-order-view.jsx
@@ -1,10 +1,33 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
+import Grid from "@mui/material/Grid";
 import PendingOrderTable from '../pending-order-table'
+import PendingPaymentVerifyTable from '../pending-payment-verify'
 
 const PendingOrderView = () => {
   return (
-    <Box flexGrow={1} >
-      <PendingOrderTable />
+    <Box sx={{ flexGrow: 1 }} >
+      <Grid container marginTop={1} spacing={2}>
+        <Grid item xs={12} md={8}>
+          <Typography
+            variant="h6"
+            marginBottom={1}
+            style={{ color: "#07bc0c", fontWeight: "bolder" }}
+          >
+            Pending Orders
+          </Typography>
+          <PendingOrderTable />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Typography
+            variant="h6"
+            marginBottom={1}
+            style={{ color: "#07bc0c", fontWeight: "bolder" }}
+          >
+            Pending Payment Verify
+          </Typography>
+          <PendingPaymentVerifyTable />
+        </Grid>
+      </Grid>
     </Box>
   )
 }
