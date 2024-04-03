f25cfbf1d38e91fc77bc1b1341ddcfcfc2ba859f
it22343048 <162109363+ChamudiImasha@users.noreply.github.com>
2024-04-03 14:51:42 +0530

    support table

---
+++ src/section/support-desk/support-table.jsx
@@ -0,0 +1,61 @@
+import { Box, Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { getSupportRequests } from '../../api/supportApi'
+
+const SupportTableView = () => {
+
+const [request,setRequest] = useState([""])
+
+useEffect(()=>{
+
+
+    getSupportRequests().then((request)=>{setRequest(request)
+    console.log(request)
+    })
+
+  },[])
+
+
+
+  return (
+    <>
+    <TableContainer component={Paper} style={{ marginBottom: '10px' }}>
+        <Table size="small">
+            <TableHead>
+                <TableRow>
+                    <TableCell style={{ fontWeight: 'bold' }}>Date</TableCell>
+                    <TableCell style={{ fontWeight: 'bold' }}>Name</TableCell>
+                    <TableCell style={{ fontWeight: 'bold' }}>Topic</TableCell>
+                    <TableCell style={{ fontWeight: 'bold' }}>Status</TableCell>
+                  
+                    <TableCell style={{ fontWeight: 'bold' }}>Actions</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                { request&&request.map((req,index) => (
+                    <TableRow key={index}>
+                        <TableCell>{req.localDate}</TableCell>
+                        <TableCell>{req.user_name}</TableCell>
+                        <TableCell>{req.topic}</TableCell>
+                        <TableCell>{req.status}</TableCell>
+                        
+                        <TableCell>
+                            <Box display="flex">
+                                {/* Link to add solution */}
+                                {/* <Link to={/AddSolution/${issue.id}}> */}
+                                    <Button variant="contained" style={{ backgroundColor: '#2CA019', color: 'white', marginRight: '8px', fontSize: '10px' }}>
+                                        Add
+                                    </Button>
+                                {/* </Link> */}
+                            </Box>
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    </TableContainer>
+</>
+  )
+}
+
+export default SupportTableView
