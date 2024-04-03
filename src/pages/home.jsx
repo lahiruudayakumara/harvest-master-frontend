23c0acd63d17b555b6da78ffc8df01f54d5d3879
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2024-04-04 00:56:27 +0530

    Update import statement for Footer component in home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from "react-helmet";
 import HomeUpperPart from "src/components/home/homeUpperPart";
 import HomeMiddlePart from "src/components/home/homeMiddlePart";
-import FooterForHome from "src/components/footer/footerForHome";
+import Footer from "src/components/footer/footer";
 
 export default function Home() {
   return (
@@ -11,7 +11,7 @@ export default function Home() {
       </Helmet>
       <HomeUpperPart />
       <HomeMiddlePart />
-      <FooterForHome />
+      <Footer />
     </>
   );
 }
