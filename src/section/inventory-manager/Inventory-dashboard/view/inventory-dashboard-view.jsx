420e88d8cd9ceb27356a80e99a08351ae6a4ce3a
N A D NIMSARA <duvindunimsara@gmail.com>
2024-04-05 14:57:53 +0530

    Remove unused import

--- src/section/inventory-manager/Inventory-dashboard/view/inventory-dashboard-view.jsx
+++ src/section/inventory-manager/Inventory-dashboard/view/inventory-dashboard-view.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ProductTable from "../product-table";
 import { Box } from "@mui/material";
-import BasicDateCalendar from "../../../../components/calender/calendar";
 
 const InventoryDashBoard = () => {
   return (
     <Box flex={1}>
-      
       <ProductTable />
     </Box>
   );
