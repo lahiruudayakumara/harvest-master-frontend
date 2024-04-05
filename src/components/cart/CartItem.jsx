48eb8904de292c595978a7bfadd4aacdc887c3e7
Dushan Dhananjaya <dushandhananjaya01@gmail.com>
2024-04-05 13:26:15 +0530

    import OrderSummary component

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -7,6 +7,7 @@ import { Add, Remove } from '@mui/icons-material';
 import axios from 'axios';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import OrderSummary from './OrderSummary';
 
 const Img = styled('img')({
   width:150,
@@ -16,17 +17,6 @@ const Img = styled('img')({
 
 const Info = styled('div')({
   flex: 3,
-})
-
-const Summary = styled('div')({
-  flex: 1,
-  margin:20,
-  padding:20,
-  marginLeft:100,
-  marginRight:0,
-  height: '100%',
-  position:'sticky',
-  top:10,
 })
 
 const ProductDetail = styled('div')({
@@ -69,37 +59,6 @@ const Amount = styled('div')({
 const ProductPrice = styled('div')({
   fontSize: 30,
   fontWeight:200
-})
-
-const Title = styled('h1')({
-  fontWeight:200,
-})
-
-const Item = styled('div')({
-  marginTop:30,
-  marginBottom: 30,
-  marginLeft:0,
-  marginRight:0,
-  display:'flex',
-  justifyContent:'space-between'
-})
-
-const ItemPrice = styled('span')({
-
-})
-
-const Text = styled('span')({
-
-})
-
-const CheckoutButton = styled('button')({
-  width:'100%',
-  padding:10,
-  backgroundColor:'#2CA019',
-  color:'white',
-  fontWeight:600,
-  cursor:'pointer',
-  border: 0,
 })
 
 const CartItem = () => {
@@ -152,20 +111,9 @@ const CartItem = () => {
               </Grid>
             ))
           }
-
+        
         </Info>
-        <Summary sx={{boxShadow:4, borderRadius:'2px'}}>
-          <Title > Order Summary </Title>
-          <Item>
-            <Text> Sub Total </Text>
-            <ItemPrice> Rs 100 </ItemPrice>
-          </Item>
-          <Item>
-            <Text sx={{fontWeight:500, fontSize:24}}> Total </Text>
-            <ItemPrice sx={{fontWeight:500, fontSize:24}}> Rs 100 </ItemPrice>
-          </Item>
-          <CheckoutButton> CHECKOUT NOW </CheckoutButton>
-        </Summary>
+        <OrderSummary/>
       </Grid>
     </>
   );
