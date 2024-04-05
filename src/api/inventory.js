68b6488af051f2aaa8c664234ea7a2b972283ca2
N A D NIMSARA <duvindunimsara@gmail.com>
2024-04-05 14:57:25 +0530

    Refactor inventory API functions

--- src/api/inventory.js
+++ src/api/inventory.js
@@ -1,25 +1,31 @@
 // Initial path: /api/inventory.js
-import axios from 'axios';
+import axios from "axios";
 
+const URL = "http://localhost:8080";
 
-const URL = 'http://localhost:8080';
-
-
-export const getInventory = async () => {
+export const getInventoryApi = async () => {
   const response = await axios.get(`${URL}/inventory/getAll`);
   return response.data;
-} 
+};
 
-export const addInventory = async (formData) => {
+export const addInventoryApi = async (formData) => {
   const response = await axios.post(`${URL}/inventory/add`, formData, {
     headers: {
-      'Content-Type': 'multipart/form-data',
+      "Content-Type": "multipart/form-data",
     },
   });
   return response.data;
-}
+};
 
-export const deleteProduct = async (id) => {
+export const updateInventoryApi = async (formData) => {
+  const response = await axios.put(
+    `${URL}/inventory/update/${formData.pid}`,
+    formData
+  );
+  return response.data;
+};
+
+export const deleteProductApi = async (id) => {
   const response = await axios.delete(`${URL}/inventory/delete/${id}`);
   return response.data;
 };
