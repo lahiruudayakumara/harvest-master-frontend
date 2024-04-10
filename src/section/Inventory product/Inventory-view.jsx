f78e8c7166f64291cfedd15ca695e43b612034b6
N A D NIMSARA <duvindunimsara@gmail.com>
2024-04-10 20:31:32 +0530

    Add Inventory-view component to display inventory in the Inventory-view.jsx file

---
+++ src/section/Inventory product/Inventory-view.jsx
@@ -0,0 +1,44 @@
+import { Grid, Paper, Typography, Box, TextField, createTheme, ThemeProvider } from "@mui/material";
+import React from "react";
+import { TopImage } from "../../components/top-section-image";
+import { InventoryProducts } from "./Inventory-product";
+
+export const Inventoryview = () => {
+
+    const theme = createTheme({
+      palette: {
+        primary: {
+          main: "#0F601F",
+        },
+      },
+      typography: {
+        // Set all text color to black
+        body1: {
+          color: "black",
+          fontWeight: "550", // Make text bold
+        },
+        // You can define other variants here as needed
+      },
+    });
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <TopImage
+          classname="community_stock"
+          title1=" PLAN AHEAD YOUR"
+          title2=" HARVESTING.
+ Find Your Plans Here."
+        ></TopImage>
+        <div>
+          <Grid container spacing={2} style={{ height: "100%" }}>
+            {/* First Column */}
+           
+
+            {/* Second Column */}
+           <InventoryProducts></InventoryProducts>
+          </Grid>
+        </div>
+      </ThemeProvider>
+    </>
+  );
+};
