5fd709812483ccbfad493b5999328d5eaf85fef5
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2024-04-10 18:42:31 +0530

    remove filters

--- src/section/logistic-handler/pending-order/view/pending-order-view.jsx
+++ src/section/logistic-handler/pending-order/view/pending-order-view.jsx
@@ -2,25 +2,9 @@ import { Box, Typography } from '@mui/material'
 import Grid from "@mui/material/Grid";
 import PendingOrderTable from '../pending-order-table'
 import PendingPaymentVerifyTable from '../pending-payment-verify'
-import { useEffect } from 'react';
-import { getPendingOrders } from 'src/api/logisticHandlerApi';
-import { selectPendingOrders, setPendingOrders } from 'src/stores/slices/pendingOrderSlice';
-import { useDispatch, useSelector } from 'react-redux';
 
 const PendingOrderView = () => {
 
-  const dispatch = useDispatch();
-
-  const filterData = {
-    "order_Status": "PENDING",
-    "payment_status": "PENDING"
-  }
-
-  useEffect(() => {
-    getPendingOrders(filterData).then((data) => {
-      dispatch(setPendingOrders(data));
-    });
-  }, []);
   return (
     <Box sx={{ flexGrow: 1 }} >
       <Grid container marginTop={1} spacing={2}>
