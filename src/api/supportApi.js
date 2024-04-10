cb530d5379a474f04cd7e2d7334cdfc74209ed3f
it22343048 <162109363+ChamudiImasha@users.noreply.github.com>
2024-04-10 11:05:41 +0530

    supportApi

--- src/api/supportApi.js
+++ src/api/supportApi.js
@@ -4,8 +4,14 @@ const URL = "http://localhost:8080";
 
 export const addSupportRequest = async (supportData) => {
     const response= await axios.post (`${URL}/support/add`, {
-        status : supportData.topic,
-        issue : supportData.description
+        topic : supportData.topic,
+      issue: supportData.description,
+        status: "Pending"
     
     })
 }
+
+export const getSupportRequests = async () => {
+  const response = await axios.get(`${URL}/support/getall`);
+  return response.data;
+};
