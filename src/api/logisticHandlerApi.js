1cedc4fc07222bfcde2cf0387b326fb09e361105
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2024-04-10 23:56:16 +0530

    make all the API s related to  logistic handler

--- src/api/logisticHandlerApi.js
+++ src/api/logisticHandlerApi.js
@@ -2,18 +2,7 @@ import axios from "axios";
 
 const URL = 'http://localhost:8080';
 
-export const getPendingOrders = async (filterData) => {
-    try {
-        const response = await axios.post(`${URL}/api/delivery/get/pending`, filterData);
-        return response.data;
-    } catch (error) {
-        // Handle error
-        console.error("Error fetching pending orders:", error);
-        throw error; // Rethrow the error to be caught by the caller
-    }
-}
-
-
+//create delivery request from buyer
 export const addOrderDelivery = async (formData) => {
     try {
         const response = await axios.post(`${URL}/api/delivery/create`, formData);
@@ -25,9 +14,45 @@ export const addOrderDelivery = async (formData) => {
     }
 }
 
+//read the pending orders(both tables in the pending order UI)
+export const getPendingOrders = async (filterData) => {
+    try {
+        const response = await axios.post(`${URL}/api/delivery/get/pending`, filterData);
+        return response.data;
+    } catch (error) {
+        // Handle error
+        console.error("Error fetching pending orders:", error);
+        throw error; // Rethrow the error to be caught by the caller
+    }
+}
 
+//edit the delivery schedule table
 export const updateDeliverySchedule = async (formData) => {
     const response = await axios.put( `${URL}/api/delivery/update/${formData.deliveryId}`, formData);
     return response.data;
   };
 
+//approve or delete the pending order
+  export const managePendingOrder = async (id) => {
+    const response = await axios.put(`${URL}/api/delivery/manage/${deliveryId}`);
+    return response.data;
+  };
+  
+export const confirmDelivery = async (formData) => {
+    const response = await axios.put( `${URL}/api/delivery/confirm/${formData.deliveryId}`, formData);
+    return response.data;
+  };
+
+//read the log activity table
+export const getLogActivity = async (filterData) => {
+    const response = await axios.get(`${URL}/api/delivery/log`, filterData);
+    return response.data;        
+       
+}
+
+
+//get total count of pending orders and deliverd orders
+export const getOrderCount = async () => {
+    const response = await axios.get(`${URL}/api/delivery/total`);
+    return response.data;
+}
