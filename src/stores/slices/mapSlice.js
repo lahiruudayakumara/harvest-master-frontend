66a4d64e3a3fb1ea1b014c577148c4ad8ad016f0
It22899224J <it22899224@my.sliit.lk>
2024-04-12 19:11:44 +0530

    Add mapSlice reducer for managing map state

---
+++ src/stores/slices/mapSlice.js
@@ -0,0 +1,40 @@
+import { red } from "@mui/material/colors"
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+
+
+const initialState = {
+    district: '',
+    loading: false,
+    error: null,
+}
+
+export const mapSlice = createSlice({
+
+    name: 'map',
+    initialState,
+    reducers: {
+        setDistrict: (state, action) => {
+            state.district = action.payload;
+        },
+        setLoading: (state, action) => {
+            state.loading = action.payload;
+        },
+        setError: (state, action) => {
+            state.error = action.payload;
+        }
+    }
+
+
+
+
+
+
+})
+
+
+export const { setDistrict, setLoading, setError } = mapSlice.actions;
+export default mapSlice.reducer;
+export const  selectMap = (state) => state.map;
