b9128c47c4ec50c72c650d8ffddc905e2bece640
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-04-14 23:17:38 +0530

    Update main.jsx to use Stripe elements for payment processing

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,17 @@ import App from './app.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './stores/store.js'
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
 
-
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Elements stripe={stripePromise}>
+        <App />
+      </Elements>
     </Provider>
   </React.StrictMode>,
 )
