5802fce2faa8e966f4ef1ba874cf548992c83571
It22899224J <it22899224@my.sliit.lk>
2024-04-16 18:22:00 +0530

    Add bidSlice.js to handle bids in the store

---
+++ src/stores/slices/bidSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  bids: [],
+};
+
+export const bidSlice = createSlice({
+  name: "bids",
+  initialState,
+    reducers: {
+        setBidsList:(state, action) =>{
+            
+            state.bids = action.payload;
+            
+        },
+        removeBid:(state, action)=> {
+            
+            state.bids = state.bids.filter(
+
+                (bid)=>bid.id !== action.payload
+            )
+        }
+  },
+});
+
+
+export const { setBidsList, removeBid } = bidSlice.actions;
+export const selectBid = (state) => state.bids;
+export default bidSlice.reducer;
