02c0bb366a533a591b37feacbf3bded441e99024
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2024-04-17 10:58:14 +0530

    Add FormControls component for handling form controls

---
+++ src/components/preHarvestForms/controls/FormControls.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import InputX from "./InputX";
+import InputAdornmentX from "./InputAdornmentX";
+import RadioGroupX from "./RadioGroupX";
+import AutocompleteX from "./AutocompleteX";
+import CheckBoxX from "./CheckBox";
+
+const FormControls = {
+  InputX,
+  InputAdornmentX,
+  RadioGroupX,
+  AutocompleteX,
+  CheckBoxX,
+};
+
+export default FormControls;
