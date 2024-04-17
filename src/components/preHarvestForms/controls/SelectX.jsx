f65e6623a68ccfbf18d9c3f29a6f3f4e58978733
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2024-04-17 10:59:27 +0530

    Refactor SelectX component to use Material-UI components

---
+++ src/components/preHarvestForms/controls/SelectX.jsx
@@ -0,0 +1,22 @@
+// import React from "react";
+// import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+
+// const SelectX = (props) => {
+//   const { name, label, value, onChange, options, style } = props;
+//   return (
+//     <div>
+//       <FormControl variant="outlined" style={style}>
+//         <InputLabel>{label}</InputLabel>
+//         <Select label={label} name={name} value={value} onChange={onChange}>
+//           {options.map((item) => (
+//             <MenuItem key={item.id} value={item.id}>
+//               {item.name}
+//             </MenuItem>
+//           ))}
+//         </Select>
+//       </FormControl>
+//     </div>
+//   );
+// };
+
+// export default SelectX;
