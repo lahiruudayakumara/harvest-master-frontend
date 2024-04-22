461f56cb4f372c15555e99f3d4613e76674c9b85
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2024-04-22 23:30:07 +0530

    Refactor SelectX component to include additional props in Select component

--- src/components/preHarvestForms/controls/SelectX.jsx
+++ src/components/preHarvestForms/controls/SelectX.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 
 const SelectX = (props) => {
-  const { name, label, value, onChange, options, style } = props;
+  const { name, label, value, onChange, options, style, ...otherProps } = props;
   return (
     <div>
       <FormControl variant="outlined" style={style}>
         <InputLabel>{label}</InputLabel>
-        <Select label={label} name={name} value={value} onChange={onChange}>
+        <Select
+          label={label}
+          name={name}
+          value={value}
+          onChange={onChange}
+          {...otherProps}
+        >
           {options.map((item) => (
             <MenuItem key={item.id} value={item.id}>
               {item.name}
