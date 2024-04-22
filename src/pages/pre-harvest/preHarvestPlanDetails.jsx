869f0d02361b00b2ff6928c97526131ff0f6fb81
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2024-04-22 16:25:29 +0530

    Refactor PreHarvestPlanDetails component to fetch and display plan details with fieldId parameter

--- src/pages/pre-harvest/preHarvestPlanDetails.jsx
+++ src/pages/pre-harvest/preHarvestPlanDetails.jsx
@@ -1,19 +1,43 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import PlanDetailsTopBar from "../../components/planDetailsPg/planDetailsTopBar";
 import PlanDetailsMiddle from "../../components/planDetailsPg/planDetailsMiddle";
 import PlanDetailsLower from "../../components/planDetailsPg/planDetailsLower";
 import PlanDetailsBottom from "../../components/planDetailsPg/planDetailsBottom";
+import ScrollToTop from "src/components/postHarvest/scroll-top";
+import { getPreHarvestPlanByIdApi } from "../../api/preHarvestApi";
 
 const PreHarvestPlanDetails = () => {
+  const { fieldId } = useParams();
+  console.log(fieldId);
+
+  const [planDetails, setPlanDetails] = useState({});
+
+  useEffect(() => {
+    const fetchPlanDetails = async () => {
+      if (!fieldId) return;
+
+      try {
+        const plan = await getPreHarvestPlanByIdApi(fieldId);
+        setPlanDetails(plan);
+        console.log(plan);
+      } catch (error) {
+        console.error("Error fetching plan details", error);
+      }
+    };
+    fetchPlanDetails();
+  }, [fieldId]);
+
   return (
     <>
       <Helmet>
         <title>plan details</title>
       </Helmet>
+      <ScrollToTop />
       <PlanDetailsTopBar />
-      <PlanDetailsMiddle />
-      <PlanDetailsLower />
+      <PlanDetailsMiddle planDetails={planDetails} />
+      <PlanDetailsLower fieldId={fieldId} />
       <PlanDetailsBottom />
     </>
   );
