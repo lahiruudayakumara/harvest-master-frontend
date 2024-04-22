c38b2d4ff37afdfbc1b93441b172f9d76c26a1d0
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-04-22 14:58:03 +0530

    Update transaction table columns and rows in TranstractionTable.jsx

--- src/section/financial-manager/account/transtraction-table.jsx
+++ src/section/financial-manager/account/transtraction-table.jsx
@@ -9,35 +9,39 @@ import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import { Box } from '@mui/material';
 import { useEffect } from 'react';
-import { fetchTransaction } from 'src/stores/slices/paymentSlice';
+import { Transaction, fetchTransaction } from 'src/stores/slices/paymentSlice';
 import { getAllTranstractionDetails } from 'src/api/financialManagerApi';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const columns = [
-    { id: 'name', label: 'Name', minWidth: 170 },
-    { id: 'account', label: 'Account\u00a0No', minWidth: 100 },
-    { id: 'date', label: 'Date', minWidth: 100 },
-    { id: 'amount', label: 'Amount(Rs)', minWidth: 100 },
-    { id: 'status', label: 'Status', minWidth: 100 },
+    { id: 'transactionDate', label: 'Date'},
+    { id: 'paymentMethod', label: 'Method'},
+    { id: 'paymentMethod', label: 'Payment Method'},
+    { id: 'totalPrice', label: 'Amount(Rs)' },
+    { id: 'status', label: 'Status'},
 ];
 
-function createData(name, account, date, amount, status) {
-    return { name, account, date, amount, status };
-}
+// function createData(name, account, date, amount, status) {
+//     return { name, account, date, amount, status };
+// }
 
-const rows = [
-    createData('Duvindu Nimsara', '4545 8787 4544 1234', '2024 - 02 - 19', '15,100.00', 'Done'),
-    createData('John Smith', '1234 5678 9012 3456', '2023-10-15', '20,500.00', 'Pending'),
-    createData('Michael Brown', '2468 1357 8024 6793', '2024-03-10', '12,300.00', 'Pending'),
-    createData('Sophia Garcia', '6543 2109 8765 4321', '2024-02-28', '18,900.00', 'Processing'),
-    createData('Daniel Martinez', '1357 2468 6793 8024', '2024-03-05', '6,500.00', 'Done'),
-    createData('Olivia Taylor', '3210 9876 5432 1098', '2024-02-14', '15,750.00', 'Rejected')
-];
+// const rows = [
+//     createData('Duvindu Nimsara', '4545 8787 4544 1234', '2024 - 02 - 19', '15,100.00', 'Done'),
+//     createData('John Smith', '1234 5678 9012 3456', '2023-10-15', '20,500.00', 'Pending'),
+//     createData('Michael Brown', '2468 1357 8024 6793', '2024-03-10', '12,300.00', 'Pending'),
+//     createData('Sophia Garcia', '6543 2109 8765 4321', '2024-02-28', '18,900.00', 'Processing'),
+//     createData('Daniel Martinez', '1357 2468 6793 8024', '2024-03-05', '6,500.00', 'Done'),
+//     createData('Olivia Taylor', '3210 9876 5432 1098', '2024-02-14', '15,750.00', 'Rejected')
+// ];
+
+
 
 export default function TranstractionTable() {
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(10);
     const dispatch = useDispatch();
+
+    const rows = useSelector(Transaction)
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
