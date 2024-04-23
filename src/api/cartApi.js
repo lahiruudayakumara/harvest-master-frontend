b2d0ed5c5f230b0417bb4a88f5de90afe461a801
Dushan Dhananjaya <dushandhananjaya01@gmail.com>
2024-04-23 20:46:31 +0530

    update api

--- src/api/cartApi.js
+++ src/api/cartApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:8080';
+const URL = 'http://localhost:8091';
 
 export const loadCartItemsApi = async () => {
     const cus_id = 1;
@@ -15,5 +15,10 @@ export const  addToCartApi = async (requestData) => {
 
 export const deleteCartItemApi = async (cart_item_id) => {
     const responce = await axios.delete(`${URL}/api/harvestMaster/cart/${cart_item_id}`)
+    return responce
+}
+
+export const addDiscountApi = async (discountDetails) => {
+    const responce = await axios.post(`${URL}/api/harvestMaster/discount`, discountDetails)
     return responce
 }
