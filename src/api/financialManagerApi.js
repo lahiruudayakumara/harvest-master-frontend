48a5dfff9398a2dcd17620c54698fcf70f2d44c0
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-04-24 23:30:44 +0530

    Add financialManagerApi functions for transaction and payment management

--- src/api/financialManagerApi.js
+++ src/api/financialManagerApi.js
@@ -11,3 +11,43 @@ export const sendTransactionDetails = async (data) => {
     const response = await axios.post(`${URL}/api/transaction/process`, data);
     return response.data;
 }
+
+export const getTransactionById = async (id) => {
+    const response = await axios.get(`${URL}/api/transaction/get-transaction/${id}`);
+    return response.data;
+}
+
+export const getLogActivityFinance = async () => {
+    const response = await axios.get(`${URL}/api/payment-log-activity`);
+    return response.data;
+}
+
+const draft = {
+    "paymentStatus": "PENDING"
+}
+
+export const getDraftPayments = async () => {
+    const response = await axios.post(`${URL}/api/payment-info/status`, draft);
+    return response.data;
+}
+
+export const getVerifyPayments = async () => {
+    const response = await axios.post(`${URL}/api/payment-info/status`, { paymentStatus: 'VERIFY'});
+    return response;
+}
+
+
+export const createPayment = async (paymentInfo, paymentStatus) => {
+const response = await axios.post(`${URL}/api/payment-info`,{ paymentInfo: paymentInfo, paymentStatus: paymentStatus });
+    return response;
+}
+
+export const deletePayment = async (id) => {
+    const response = await axios.delete(`${URL}/api/payment-info/${id}`);
+    return response;
+}
+
+export const updatePayment = async (id, data) => {
+    const response = await axios.put(`${URL}/api/payment-info/${id}`, data);
+    return response;
+}
