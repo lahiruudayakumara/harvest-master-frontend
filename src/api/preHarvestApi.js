e6b972a214625f8169b47e7f44f4df4d9e77a072
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2024-04-24 10:37:02 +0530

    Add API functions for field visit requests and pre-harvest costs

--- src/api/preHarvestApi.js
+++ src/api/preHarvestApi.js
@@ -48,3 +48,103 @@ export const deletePreHarvestPlanApi = async (fieldId) => {
     throw error;
   }
 };
+
+export const addFieldVisitRequestApi = async (id, formData) => {
+  const response = await axios.post(
+    `${URL}/PreHarvestFieldVisits/add/${id}`,
+    formData,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return response.data;
+};
+
+export const getAllFieldVisitRequestsApi = async (id) => {
+  const response = await axios.get(`${URL}/PreHarvestFieldVisits/getAll/${id}`);
+  return response.data;
+};
+
+export const getFieldVisitRequestByIdApi = async (id) => {
+  const response = await axios.get(`${URL}/PreHarvestFieldVisits/get/${id}`);
+  return response.data;
+};
+
+export const updateFieldVisitRequestByIdApi = async (
+  id,
+  reqId,
+  updatedPlan
+) => {
+  try {
+    const response = await axios.put(
+      `${URL}/PreHarvestFieldVisits/update/${id}/${reqId}`,
+      updatedPlan
+    );
+    return response;
+  } catch (error) {
+    console.error("Error updating Field Visit Request:", error);
+    throw error;
+  }
+};
+
+export const deleteFieldVisitRequestByIdApi = async (reqId) => {
+  try {
+    const response = await axios.delete(
+      `${URL}/PreHarvestFieldVisits/delete/${reqId}`
+    );
+    return response;
+  } catch (error) {
+    console.error("Error deleting Field Visit Request:", error);
+    throw error;
+  }
+};
+
+export const addPreHarvestCostApi = async (id, formData) => {
+  const response = await axios.post(
+    `${URL}/preHarvestCosts/add/${id}`,
+    formData,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return response.data;
+};
+
+export const getAllPreHarvestCostsApi = async (id) => {
+  const response = await axios.get(`${URL}/preHarvestCosts/getAll/${id}`);
+  return response.data;
+};
+
+export const getPreHarvestCostByIdApi = async (id) => {
+  const response = await axios.get(`${URL}/preHarvestCosts/get/${id}`);
+  return response.data;
+};
+
+export const updatePreHarvestCostByIdApi = async (id, costId, updatedPlan) => {
+  try {
+    const response = await axios.put(
+      `${URL}/preHarvestCosts/update/${id}/${costId}`,
+      updatedPlan
+    );
+    return response;
+  } catch (error) {
+    console.error("Error updating PreHarvest Cost:", error);
+    throw error;
+  }
+};
+
+export const deletePreHarvestCostByIdApi = async (costId) => {
+  try {
+    const response = await axios.delete(
+      `${URL}/preHarvestCosts/delete/${costId}`
+    );
+    return response;
+  } catch (error) {
+    console.error("Error deleting PreHarvest Cost:", error);
+    throw error;
+  }
+};
