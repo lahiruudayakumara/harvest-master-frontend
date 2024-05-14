5bcd5a35f15cd266be473448b7a182aac3b3fe41
It22899224J <it22899224@my.sliit.lk>
2024-05-14 15:21:38 +0530

    Refactor stock-cart-view.jsx to improve code readability and maintainability

--- src/pages/paddystock-cart/stock-cart-view.jsx
+++ src/pages/paddystock-cart/stock-cart-view.jsx
@@ -18,39 +18,80 @@ const StockCartView = () => {
 
 
   return (
-    <Grid container spacing={4} mt={15} p={5}>
-      <Grid item xs={4} sx={{backgroundColor:"green"}}>
+    <Grid
+      container
+      spacing={10}
+      mt={10}
+      pl={4}
+      pr={4}
+      sx={{ backgroundColor: "green" }}
+    >
+      <Grid item xs={6}>
+        <Typography variant="h4" mb={5} fontWeight={600}>
+          Current Bids
+        </Typography>
         <Grid container spacing={4}>
           {[1, 2, 3].map((item) => (
-            <Grid item xs={12} key={item}>
-              <Card style={{ height: 300 }} elevation={6}>
-                <CardContent>
-                  <Typography variant="h6" component="div">
-                    Item {item} in Left Section
-                  </Typography>
-                  <Typography variant="body2">
-                    Description of item {item}
-                  </Typography>
-                </CardContent>
-              </Card>
+            <Grid item xs={12} key={item} display={"flex"} gap={2}>
+              <Grid item xs={10} key={item}>
+                <Card style={{ height: 300 }} elevation={6}>
+                  <CardContent>
+                    <Typography variant="h6" component="div">
+                      Item {item} in Left Section
+                    </Typography>
+                    <Typography variant="body2">
+                      Description of item {item}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={2} key={item}>
+                <Card style={{ height: 300 }} elevation={6}>
+                  <CardContent>
+                    <Typography variant="h6" component="div">
+                      Item {item} in Left Section
+                    </Typography>
+                    <Typography variant="body2">
+                      Description of item {item}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
             </Grid>
           ))}
         </Grid>
       </Grid>
-      <Grid item xs={8}>
+      <Grid item xs={6}>
+        <Typography variant="h4" mb={5} fontWeight={600}>
+          Purchase PaddyStock
+        </Typography>
         <Grid container spacing={4}>
-          {[4, 5, 6].map((item) => (
-            <Grid item xs={12} key={item}>
-              <Card style={{ width: "100%" ,height:300}} elevation={6}>
-                <CardContent>
-                  <Typography variant="h6" component="div">
-                    Item {item} in Right Section
-                  </Typography>
-                  <Typography variant="body2">
-                    Description of item {item}
-                  </Typography>
-                </CardContent>
-              </Card>
+          {[1, 2, 3].map((item) => (
+            <Grid item xs={12} key={item} display={"flex"} gap={2}>
+              <Grid item xs={10} key={item}>
+                <Card style={{ height: 300 }} elevation={6}>
+                  <CardContent>
+                    <Typography variant="h6" component="div">
+                      Item {item} in Left Section
+                    </Typography>
+                    <Typography variant="body2">
+                      Description of item {item}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={2} key={item}>
+                <Card style={{ height: 300 }} elevation={6}>
+                  <CardContent>
+                    <Typography variant="h6" component="div">
+                      Item {item} in Left Section
+                    </Typography>
+                    <Typography variant="body2">
+                      Description of item {item}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
             </Grid>
           ))}
         </Grid>
