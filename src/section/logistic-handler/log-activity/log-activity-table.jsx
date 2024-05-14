766f7a744b060e76d39a7af16cba11e7927d25ed
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2024-05-14 09:24:55 +0530

    modify log activity

--- src/section/logistic-handler/log-activity/log-activity-table.jsx
+++ src/section/logistic-handler/log-activity/log-activity-table.jsx
@@ -6,7 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import { Box } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getLogActivity } from 'src/api/logisticHandlerApi';
@@ -56,56 +56,123 @@ export default function LogActivityTable() {
     const rows = useSelector(selectLogActivity);
 
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-                <TableContainer sx={{ maxHeight: 440 }}>
-                    <Table stickyHeader aria-label="sticky table">
-                        <TableHead>
-                            <TableRow>
-                                {columns.map((column) => (
-                                    <TableCell
-                                        key={column.id}
-                                        align={column.align}
-                                        style={{ minWidth: column.minWidth }}
-                                    >
-                                        {column.label}
-                                    </TableCell>
-                                ))}
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {rows
-                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                .map((row) => {
-                                    return (
-                                        <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                                            {columns.map((column) => {
-                                                const value = row[column.id];
-                                                return (
-                                                    <TableCell key={column.id} align={column.align}>
-                                                        {column.format && typeof value === 'number'
-                                                            ? column.format(value)
-                                                            : value}
-                                                    </TableCell>
-                                                );
-                                            })}
-                                        </TableRow>
-                                    );
-                                })}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <TablePagination
-                    rowsPerPageOptions={[10, 25, 100]}
-                    component="div"
-                    count={rows.length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onPageChange={handleChangePage}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                />
-            </Paper>
-        </Box>
-
+        <Grid sx={{ width: "100%" }}>
+            <Box display="flex" sx={{ justifyContent: 'space-between' }}>
+                <Box sx={{ display: 'flex' }}>
+                    {/* <Typography>Search Date : {selectedDate == null ? '' : selectedDate.format('YYYY-MM-DD')} </Typography> */}
+                    <Box sx={{ marginRight: 2 }}>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DatePicker
+                                // onChange={handleDateChange}
+                                // value={selectedDate}
+                                renderInput="date"
+                            />
+                        </LocalizationProvider>
+                    </Box>
+                    <FormControl sx={{ minWidth: 180 }}>
+                        <InputLabel id="demo-simple-select-label">Filter</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={selectedOption}
+                            onChange={handleChange}
+                        >
+                            <MenuItem value="option1">ALL</MenuItem>
+                            <MenuItem value="option2">PENDING</MenuItem>
+                            <MenuItem value="option3">VERIFY</MenuItem>
+                            <MenuItem value="option4">REFUND</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <FormControl sx={{ minWidth: 180 }}>
+                        <InputLabel id="demo-simple-select-label">Filter</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={selectedMethod}
+                            onChange={handleFilterMethod}
+                        >
+                            <MenuItem value="option1">ALL</MenuItem>
+                            <MenuItem value="option2">SLIP</MenuItem>
+                            <MenuItem value="option3">CARD</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Box>
+                <Box>
+                    <Button
+                        variant="contained"
+                        // onClick={handleReset}
+                        style={{ marginTop: 2, marginRight: 2, backgroundColor: '#fff', color: '#2CA019', borderColor: '#2CA019' }}
+                    >
+                        <RestartAltIcon />
+                    </Button>
+                    <Button
+                        color="success"
+                        variant="contained"
+                        // onClick={downloadReport}
+                        style={{ marginTop: 2, backgroundColor: '#2CA019' }}
+                    >
+                        <img src={excelIcon} alt="Download" style={{ width: '20px', marginRight: '5px' }} />
+                    </Button>
+                    <Button
+                        color="success"
+                        variant="contained"
+                        // onClick={generatePDF}
+                        style={{ marginTop: 2, backgroundColor: '#2CA019' }}
+                    >
+                        <img src={pdfIcon} alt="Download" style={{ width: '20px', marginRight: '5px' }} />
+                    </Button>
+                </Box>
+            </Box>
+            <Box sx={{ flexGrow: 1 }}>
+                <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+                    <TableContainer sx={{ maxHeight: 440 }}>
+                        <Table stickyHeader aria-label="sticky table">
+                            <TableHead>
+                                <TableRow>
+                                    {columns.map((column) => (
+                                        <TableCell
+                                            key={column.id}
+                                            align={column.align}
+                                            style={{ minWidth: column.minWidth }}
+                                        >
+                                            {column.label}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows
+                                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                    .map((row) => {
+                                        return (
+                                            <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                                {columns.map((column) => {
+                                                    const value = row[column.id];
+                                                    return (
+                                                        <TableCell key={column.id} align={column.align}>
+                                                            {column.format && typeof value === 'number'
+                                                                ? column.format(value)
+                                                                : value}
+                                                        </TableCell>
+                                                    );
+                                                })}
+                                            </TableRow>
+                                        );
+                                    })}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <TablePagination
+                        rowsPerPageOptions={[10, 25, 100]}
+                        component="div"
+                        count={rows.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                    />
+                </Paper>
+            </Box>
+        </Grid>
     );
 }
